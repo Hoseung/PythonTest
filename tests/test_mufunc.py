949bd4f90e6ec96db89ff6021e3b1cfa78488b24
Hoseung Choi <hschoi@dinsight.ai>
2023-11-17 08:42:44 +0900

    Zero Div & String addtion

--- tests/test_mufunc.py
+++ tests/test_mufunc.py
@@ -3,13 +3,16 @@ import src.myfunc as myfunc
 
 def test_add():
     assert myfunc.add(7, 3) == 10
-    assert myfunc.add(7) == 9
-    assert myfunc.add(5) == 7 
+    assert myfunc.add(5, 2) == 7 
 
+def test_add_strings():
+    result = myfunc.add('Hello', ' World')
+    assert result == 'Hello World'
+    assert type(result) is str
+    assert 'Heldlo' not in result
 
 def test_divide():
     assert myfunc.divide(10, 5) == 2
     assert myfunc.divide(10, 2) == 5
-    assert myfunc.divide(10) == 5
-    with pytest.raises(TypeError):
+    with pytest.raises(ZeroDivisionError):
         myfunc.divide(10, 0)
